1c29537422457ade6690dee18c24439635cd7dce
Burak Gündüz <burakkgunduz21@gmail.com>
2021-05-20 18:14:22 +0300

    home page became responsive

--- components/HomePageImage.js
+++ components/HomePageImage.js
@@ -1,7 +1,10 @@
+import styles from "../styles/HomePageImage.module.css";
+
 const HomePageImage = () => {
   return (
     <div>
       <img
+        className={styles.image}
         src="./images/happy_music.svg"
         alt="bird figure listening music with headphones"
       />
